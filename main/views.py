783ce38fcb397d0b7361a41deb0802e398bf7371
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-27 19:46:29 +0100

    a

--- main/views.py
+++ main/views.py
@@ -10,11 +10,19 @@ from .forms import NewUserForm
 def single_slug(request, single_slug):
     categories = [c.category_slug for c in TutorialCategory.objects.all()]
     if single_slug in categories:
-        return HttpResponse(f"{single_slug} is a category")
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request,
+                      "main/category.html",
+                      {"part_ones": series_urls})
 
     tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
-    if single_slug in categories:
-        return HttpResponse(f"{single_slug} is a category")
+    if single_slug in tutorials:
+        return HttpResponse(f"{single_slug} is a tutorial")
 
     return HttpResponse(f"{single_slug} does not correspond to anything")
 
