07de386128eefa51ad65545698661e81dc50b481
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-27 19:11:09 +0100

    up

--- main/views.py
+++ main/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
 
-# Create your views here.
+
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        return HttpResponse(f"{single_slug} is a category")
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in categories:
+        return HttpResponse(f"{single_slug} is a category")
+
+    return HttpResponse(f"{single_slug} does not correspond to anything")
+
+
+
 def homepage(request):
     return render(request=request,
                   template_name="main/home.html",
